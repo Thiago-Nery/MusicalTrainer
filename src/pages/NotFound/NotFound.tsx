487f40fd946fc103428afe3e2d4bde135238f42e
Thiago Nery <t.nfabricio@gmail.com>
2025-11-09 22:33:20 -0300

    updated not found page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,3 +1,18 @@
+import { Link } from "react-router-dom"
+import { Typography } from "antd"
+
+const { Title } = Typography;   
+
+
 export default function NotFound(){
-    return <>404 - Not Found!</>
+    return (
+        <div style={{padding: 20}}>
+            <Title level={2}>🎵 Musical Trainer informa:</Title>
+            <Title>Ops! Ainda estamos aprendendo esse assunto...</Title>
+            <Title>
+                <Link to="/"> Voltar a praticar ←</Link>
+            </Title>
+        </div>
+
+    )
 }
